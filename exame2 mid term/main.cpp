382bc40d766624d61e2f6eab49a0d7f02f976bd9
Vanderlei <vanderleijr2001@gmail.com>
2024-11-06 20:15:02 -0800

    Initial Commit

---
+++ exame2 mid term/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  exame2 mid term
+//
+//  Created by Vanderlei Junior on 11/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
